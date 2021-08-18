60c1aff4939d1b1b47b020353053c57ffba10eee
maddirichards <maddi.richards@keyin.com>
2021-08-18 11:40:45 -0230

    initial commit starting new project

--- MAEAFinalProject.py
+++ MAEAFinalProject.py
@@ -1,1 +1,11 @@
-print("hello")
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 1, 2, 3, 4, 5]
+
+
+plt.plot(x, y, color = "red", marker = "x")
+plt.xlabel("Time (m)")
+plt.ylabel("Speed (m/s)")
+plt.title("Graph Title")
+plt.show()
